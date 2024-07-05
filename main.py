cbf4fc0c068255385847191ad2e5c6c2b3f3c1c6
Wenkai Li <wenkai.li@ing.com>
2024-07-05 15:39:45 +0200

    dummy middleware

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import sys
 
 from powercfg import powercfg_daemon
 from powermetrics import powermetrics_daemon
+from push import push_powermetrics
 
 if __name__ == '__main__':
     system = sys.platform.lower()
     if system.startswith("darwin"):
         print('try powermetrics')
-        powermetrics_daemon(lambda x: print(x), report_interval=5)
+        # powermetrics_daemon(lambda x: print(x), report_interval=5)
+        powermetrics_daemon(lambda x: push_powermetrics(x), report_interval=5)
     else:
         print('try powercfg')
         powercfg_daemon(lambda x: print(x), report_interval=5)
